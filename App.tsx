ae7d21d9e0c50c29f356b8766f3d1bc08c83edda
Madhavi Thurlapati <madhavi.thurlapati@revature.net>
2021-02-18 11:21:16 -0700

    some error fixes

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './store/store';
 
@@ -7,11 +7,11 @@ export default function App() {
  
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-    </Provider>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  </Provider>
   );
 }
 
