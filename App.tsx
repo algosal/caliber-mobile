70a0b439a9af7846a365b80c06e2138829acee0a
Madhavi Thurlapati <madhavi.thurlapati@revature.net>
2021-02-18 11:27:57 -0700

    pulled router

--- App.tsx
+++ App.tsx
@@ -2,27 +2,19 @@ import React from 'react';
 import { StatusBar, StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './store/store';
-import {Router} from './router/router';
+import Router from './router/router';
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
  
   return (
     <Provider store={store}>
-<<<<<<< HEAD
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  </Provider>
-=======
       <View style={styles.container}>
         <NavigationContainer>
           <Router />
         </NavigationContainer>
       </View>
     </Provider>
->>>>>>> 684fc08769397235de672049e04764013a5a6404
   );
 }
 
