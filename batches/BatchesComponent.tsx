d83e4edb823a2256a494da8b5a6abad06bd4c3b1
Tngtater9 <jtater1@gmail.com>
2021-02-22 11:47:59 -0500

    added service for all batches

--- batches/BatchesComponent.tsx
+++ batches/BatchesComponent.tsx
@@ -11,71 +11,76 @@ import batchService from './BatchService';
 import Batch from './Batch';
 
 export default function BatchesComponent() {
-    const navigation = useNavigation();
-    const dispatch = useDispatch();
-    const batches = useSelector((state: BatchState) => state.batches);
-    const [filter, setFilter] = useState('');
-    const [list, setList] = useState<Batch[]>([]);
-    const keyExtractor = (item: object, index: number) => { return index.toString(); }
+	const navigation = useNavigation();
+	const dispatch = useDispatch();
+	const batches = useSelector((state: BatchState) => state.batches);
+	const [filter, setFilter] = useState('');
+	const [list, setList] = useState<Batch[]>([]);
+	const keyExtractor = (item: object, index: number) => {
+		return index.toString();
+	};
 
-    const trainerEmail = 'mock1027.employee74df14df-5842-4811-a57c-be9836537a40@mock.com';
+	const trainerEmail =
+		'mock1027.employee74df14df-5842-4811-a57c-be9836537a40@mock.com';
 
-    const batchPreview = (params: any) => {
-        return (
-            <View>
-                <Text>{params.item.name}</Text>
-                <Text>{params.item.skill}</Text>
-                <Text>{params.item.type}</Text>
-                <Text>{params.item.location}</Text>
-                <Text>{params.item.startDate}</Text>
-                <Text>{params.item.endDate}</Text>
-            </View>
-        )
-    }
+	const batchPreview = (params: any) => {
+		return (
+			<View>
+				<Text>{params.item.name}</Text>
+				<Text>{params.item.skill}</Text>
+				<Text>{params.item.type}</Text>
+				<Text>{params.item.location}</Text>
+				<Text>{params.item.startDate}</Text>
+				<Text>{params.item.endDate}</Text>
+			</View>
+		);
+	};
 
-    function handleFilter(newBatchId: any) {
-        setFilter(newBatchId);
-        if(newBatchId){
-            setList(batches.filter(batch=>batch.batchId == newBatchId));
-        } else {
-            setList(batches);
-        }
-    }
+	function handleFilter(newBatchId: any) {
+		setFilter(newBatchId);
+		if (newBatchId) {
+			setList(batches.filter((batch) => batch.batchId == newBatchId));
+		} else {
+			setList(batches);
+		}
+	}
 
-    useEffect(() => {
-        batchService.getBatchesByTrainerEmail(trainerEmail).then((batchesResp) => {
-            dispatch(getBatches(batchesResp));
-        });
-    }, []);
+	useEffect(() => {
+		batchService.getBatchesByTrainerEmail(trainerEmail).then((batchesResp) => {
+			dispatch(getBatches(batchesResp));
+		});
+	}, []);
 
-    return (
-        <View>
-            {(() => {
-                if (batches) {
-                    return (
-                        <View>
-                            <Picker
-                                selectedValue={filter}
-                                onValueChange={handleFilter}>
-                                <Picker.Item
-                                    label='None'
-                                    value=''/>
-                                {batches.length > 0 && batches.map((batch) => {
-                                    return <Picker.Item
-                                        key={batch.batchId}
-                                        label={batch.name}
-                                        value={batch.batchId}/>
-                                })}
-                            </Picker>
-                            <FlatList data={filter == '' ? null : list} renderItem={batchPreview} keyExtractor={keyExtractor}/>
-                        </View>
-                    )
-                } else {
-                    return (
-                        <ActivityIndicator/>
-                    )
-                }
-            })()}
-        </View>
-    )
+	return (
+		<View>
+			{(() => {
+				if (batches) {
+					return (
+						<View>
+							<Picker selectedValue={filter} onValueChange={handleFilter}>
+								<Picker.Item label="None" value="" />
+								{batches.length > 0 &&
+									batches.map((batch) => {
+										return (
+											<Picker.Item
+												key={batch.batchId}
+												label={batch.name}
+												value={batch.batchId}
+											/>
+										);
+									})}
+							</Picker>
+							<FlatList
+								data={filter == '' ? null : list}
+								renderItem={batchPreview}
+								keyExtractor={keyExtractor}
+							/>
+						</View>
+					);
+				} else {
+					return <ActivityIndicator />;
+				}
+			})()}
+		</View>
+	);
 }
