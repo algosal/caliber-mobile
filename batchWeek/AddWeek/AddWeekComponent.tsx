00f3b8c7be5cb549c78247f43e64104f7730ec4d
Cassandra Terry <cassandra34terry@gmail.com>
2021-02-23 17:56:28 -0600

    add week action and reducer

--- batchWeek/AddWeek/AddWeekComponent.tsx
+++ batchWeek/AddWeek/AddWeekComponent.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import { View, Button } from 'react-native';
+import QcWeek from '../QcWeek';
+import { addWeek } from '../../store/actions';
+import { useDispatch } from 'react-redux';
+import AddWeekService from './AddWeekService';
 
 
 function AddWeek(){
+    const dispatch = useDispatch();
+
+    function addWeekHandler(){
+        dispatch(addWeek(new QcWeek()));
+        AddWeekService.addWeek(new QcWeek()).then(()=> '');
+    }
+
     return (
         <View>
             <Button title='+' onPress={()=>''}></Button>
--- batchWeek/AddWeek/Week.ts
+++
@@ -1,4 +0,0 @@
-export default class Week {
-    //what goes in here. components that make up a week
-    //or just what goes in the database
-}
